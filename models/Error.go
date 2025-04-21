76e07ed04ddf6ed12e60a032413ecad4208ccae0
HOME-PC\Илья <iluxx_ivanov@mail.ru>
2025-04-21 16:38:29 +0400

    added some changes

--- models/Error.go
+++ models/Error.go
@@ -1,5 +1,13 @@
 package models
 
+import "errors"
+
+var (
+	ErrNotFound     = errors.New("not found")
+	ErrUnauthorized = errors.New("unauthorized")
+	ErrForbidden    = errors.New("forbidden")
+)
+
 type ErrorResponse struct {
 	Error      string `json:"error"`
 	StatusCode int    `json:"status_code"`
